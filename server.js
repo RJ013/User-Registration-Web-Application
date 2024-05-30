a6a44a6d09584c22c0f55827e7957ae2a693fe63
Rohit jaiswal <rohitjaiswaloffical@gmail.com>
2024-05-30 13:04:31 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,72 @@
+var express = require('express');
+var dotenv = require('dotenv').config();
+var ejs = require('ejs');
+var path = require('path');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var session = require('express-session');
+var MongoStore = require('connect-mongo');
+
+// Create an Express application
+var app = express();
+
+// Connect to MongoDB
+mongoose.connect('mongodb://localhost:27017/productDB', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}, (err) => {
+  if (!err) {
+    console.log('MongoDB Connection Succeeded.');
+  } else {
+    console.log('Error in DB connection: ' + err);
+  }
+});
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {});
+
+// Set up session management with MongoDB store
+app.use(session({
+  secret: 'work hard',
+  resave: true,
+  saveUninitialized: false,
+  store: MongoStore.create({
+    mongoUrl: 'mongodb://localhost:27017/productDB',
+    // Additional options if needed
+  })
+}));
+
+// Set view engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// Use body-parser middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Serve static files
+app.use(express.static(path.join(__dirname, 'views')));
+
+// Set up routes
+var index = require('./routes/index');
+app.use('/', index);
+
+// Catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  var err = new Error('File Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// Error handler
+app.use(function (err, req, res, next) {
+  res.status(err.status || 500);
+  res.send(err.message);
+});
+
+// Start the server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, function () {
+  console.log('Server is started on http://127.0.0.1:' + PORT);
+});
